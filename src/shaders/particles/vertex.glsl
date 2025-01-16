30e601ee97806d419ad333334f124f95ac2c4c01
raygaeta <raymondgaeta@gmail.com>
2025-01-16 10:10:23 -0800

    first commit

---
+++ src/shaders/particles/vertex.glsl
@@ -0,0 +1,79 @@
+uniform vec2 uResolution;
+uniform sampler2D uPictureTexture;
+uniform sampler2D uDisplacementTexture;
+uniform float uTime;
+
+attribute float aIntensity;
+attribute float aAngle;
+
+varying vec3 vColor;
+
+vec3 permute(vec3 x) { return mod(((x*34.0)+1.0)*x, 289.0); }
+
+float snoise(vec2 v){
+  const vec4 C = vec4(0.211324865405187, 0.366025403784439,
+           -0.577350269189626, 0.024390243902439);
+  vec2 i  = floor(v + dot(v, C.yy) );
+  vec2 x0 = v -   i + dot(i, C.xx);
+  vec2 i1;
+  i1 = (x0.x > x0.y) ? vec2(1.0, 0.0) : vec2(0.0, 1.0);
+  vec4 x12 = x0.xyxy + C.xxzz;
+  x12.xy -= i1;
+  i = mod(i, 289.0);
+  vec3 p = permute( permute( i.y + vec3(0.0, i1.y, 1.0 ))
+  + i.x + vec3(0.0, i1.x, 1.0 ));
+  vec3 m = max(0.5 - vec3(dot(x0,x0), dot(x12.xy,x12.xy),
+    dot(x12.zw,x12.zw)), 0.0);
+  m = m*m ;
+  m = m*m ;
+  vec3 x = 2.0 * fract(p * C.www) - 1.0;
+  vec3 h = abs(x) - 0.5;
+  vec3 ox = floor(x + 0.5);
+  vec3 a0 = x - ox;
+  m *= 1.79284291400159 - 0.85373472095314 * ( a0*a0 + h*h );
+  vec3 g;
+  g.x  = a0.x  * x0.x  + h.x  * x0.y;
+  g.yz = a0.yz * x12.xz + h.yz * x12.yw;
+  return 130.0 * dot(m, g);
+}
+
+void main()
+{
+    // Displacement
+    vec3 newPosition = position;
+    float displacementIntensity = texture(uDisplacementTexture, uv).r;
+    displacementIntensity = smoothstep(0.1, 0.3, displacementIntensity);
+
+    vec3 displacement = vec3(
+        cos(aAngle + uTime) * 0.2,
+        sin(aAngle + uTime) * 0.2,
+        1.0
+    );
+    displacement = normalize(displacement);
+    displacement *= displacementIntensity;
+    displacement *= 3.0;
+    displacement *= aIntensity;
+    
+    // Adding noise for liquid effect
+    float noise = snoise(vec2(newPosition.x * 0.1 + uTime * 0.1, newPosition.y * 0.1 + uTime * 0.1));
+    displacement.x += noise * 0.5;
+    displacement.y += noise * 0.5;
+
+    newPosition += displacement;
+
+    // Final position
+    vec4 modelPosition = modelMatrix * vec4(newPosition, 1.0);
+    vec4 viewPosition = viewMatrix * modelPosition;
+    vec4 projectedPosition = projectionMatrix * viewPosition;
+    gl_Position = projectedPosition;
+
+    // Picture
+    float pictureIntensity = texture(uPictureTexture, uv).r;
+
+    // Point size
+    gl_PointSize = 0.15 * pictureIntensity * uResolution.y;
+    gl_PointSize *= (1.0 / - viewPosition.z);
+
+    // Varyings
+    vColor = vec3(pow(pictureIntensity, 2.0));
+}
